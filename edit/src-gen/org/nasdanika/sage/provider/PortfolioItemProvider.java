9444c004d14038c7fae3a20a578509729c3279f7
Pavel Vlasov <Pavel.Vlasov@nasdanika.org>
2019-07-20 16:51:14 -0400

    getText() no type if name is set.

--- edit/src-gen/org/nasdanika/sage/provider/PortfolioItemProvider.java
+++ edit/src-gen/org/nasdanika/sage/provider/PortfolioItemProvider.java
@@ -120,13 +120,12 @@ public class PortfolioItemProvider extends AbstractProductItemProvider {
 	 * This returns the label text for the adapted class.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @generated
+	 * @generated NOT
 	 */
 	@Override
 	public String getText(Object object) {
 		String label = ((Portfolio) object).getName();
-		return label == null || label.length() == 0 ? getString("_UI_Portfolio_type")
-				: getString("_UI_Portfolio_type") + " " + label;
+		return label == null || label.length() == 0 ? getString("_UI_Portfolio_type") : label;
 	}
 
 	/**
