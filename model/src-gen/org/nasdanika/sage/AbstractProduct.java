583be48f56dab0ca73db8f62bb645159e33f95e3
Tatyana <tanchora@gmail.com>
2019-07-26 12:11:45 +1000

    Русификация 26.07

--- model/src-gen/org/nasdanika/sage/AbstractProduct.java
+++ model/src-gen/org/nasdanika/sage/AbstractProduct.java
@@ -14,6 +14,7 @@ package org.nasdanika.sage;
  *
  * @see org.nasdanika.sage.SagePackage#getAbstractProduct()
  * @model abstract="true"
+ *        annotation="urn:org.nasdanika label_ru='\u0410\u0431\u0441\u0442\u0440\u0430\u043a\u0442\u043d\u044b\u0439 \u043f\u0440\u043e\u0434\u0443\u043a\u0442' documentation_ru='\u0411\u0430\u0437\u043e\u0432\u044b\u0439 \u043a\u043b\u0430\u0441\u0441 \u0434\u043b\u044f \u043f\u0440\u043e\u0434\u0443\u043a\u0442\u043e\u0432 \u0438 \u043f\u043e\u0440\u0442\u0444\u0435\u043b\u0435\u0439 \u043f\u0440\u043e\u0434\u0443\u043a\u0442\u043e\u0432'"
  * @generated
  */
 public interface AbstractProduct extends ModelElement {
